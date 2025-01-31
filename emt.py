5ea337102606693a4f1bc95df301abdebe76022c
DevAgarwal2003 <142967579+DevAgarwal2003@users.noreply.github.com>
2025-02-01 01:20:04 +0530

    Update emt.py

--- emt.py
+++ emt.py
@@ -55,7 +55,7 @@ def scrape_emt(source, destination, travel_date):
         chrome_options.add_argument(option)
         
     # Set up ChromeDriver
-    service = Service('chromedriver.exe')
+    service = Service('https://github.com/DevAgarwal2003/web-scraping/blob/main/chromedriver.exe')
 
     # Create the WebDriver instance with the Service
     driver = webdriver.Chrome(options = chrome_options, service=service)
